e59c6ff668823caf5ef3897b845441d748ffd6c0
jerome.garcia <jerome.je.garcia@gmail.com>
2022-07-04 22:38:13 +0200

    feat: add kafka consumer

---
+++ kafka/src/main.rs
@@ -0,0 +1,109 @@
+use clap::{App, Arg};
+
+use rdkafka::{ClientConfig, ClientContext, Message, TopicPartitionList};
+use rdkafka::config::RDKafkaLogLevel;
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance, StreamConsumer};
+use rdkafka::error::KafkaResult;
+use rdkafka::util::get_rdkafka_version;
+
+struct CustomContext;
+
+impl ClientContext for CustomContext {}
+
+impl ConsumerContext for CustomContext {
+    fn pre_rebalance(&self, rebalance: &Rebalance) {
+        println!("Pre rebalance {:?}", rebalance);
+    }
+
+    fn post_rebalance(&self, rebalance: &Rebalance) {
+        println!("Post rebalance {:?}", rebalance);
+    }
+
+    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
+        println!("Committing offsets: {:?}", result);
+    }
+}
+
+type LoggingConsumer = StreamConsumer<CustomContext>;
+
+async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
+    let context = CustomContext;
+
+    let consumer: LoggingConsumer = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", brokers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        //.set("statistics.interval.ms", "30000")
+        //.set("auto.offset.reset", "smallest")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create_with_context(context)
+        .expect("Consumer creation failed");
+
+    consumer
+        .subscribe(&topics.to_vec())
+        .expect("Can't subscribe to specified topics");
+
+    loop {
+        match consumer.recv().await {
+            Err(e) => println!("Kafka error: {}", e),
+            Ok(m) => {
+                let payload = match m.payload_view::<str>() {
+                    None => "",
+                    Some(Ok(s)) => s,
+                    Some(Err(e)) => {
+                        println!("Error while deserializing message payload: {:?}", e);
+                        ""
+                    }
+                };
+                println!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                      m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
+                // if let Some(headers) = m.headers() {
+                    // for header in headers.iter() {
+                    //     println!("  Header {:#?}: {:?}", header.key, header.value);
+                    // }
+                // }
+                consumer.commit_message(&m, CommitMode::Async).unwrap();
+            }
+        };
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let matches = App::new("consumer")
+        .arg(
+            Arg::with_name("brokers")
+                .short("b")
+                .long("brokers")
+                .takes_value(true)
+                .default_value("localhost:9092")
+        )
+        .arg(
+            Arg::with_name("group-id")
+                .short("g")
+                .long("group-id")
+                .takes_value(true)
+                .default_value("test")
+        ).arg(
+        Arg::with_name("topics")
+            .short("t")
+            .long("topics")
+            .help("Topic list")
+            .takes_value(true)
+            .multiple(true)
+            .default_value("quickstart")
+            .required(true)
+        )
+        .get_matches();
+
+    let (version_n, version_s) = get_rdkafka_version();
+    println!("rd_kafka_version: 0x{:08x}, {}", version_n, version_s);
+    let topics = matches.values_of("topics").unwrap().collect::<Vec<&str>>();
+    let brokers = matches.value_of("brokers").unwrap();
+    let group_id = matches.value_of("group-id").unwrap();
+
+    consume_and_print(brokers, group_id, &topics).await
+}
+
